6fbd0120311781f0d36ffefd340ebf096a2406f6
Mahmudxon <mahmudxon0205@gmail.com>
2023-10-30 17:35:24 +0500

    1 commit

--- lib/assets/constants/icons.dart
+++ lib/assets/constants/icons.dart
@@ -15,4 +15,17 @@ sealed class AppIcons {
   static const notification = '${_tasks}notification.svg';
   static const search = '${_tasks}search.svg';
   static const filter = '${_tasks}filter.svg';
+  static const gym = '${_tasks}gym.svg';
+  static const sun = '${_tasks}mode.svg';
+  static const star = '${_tasks}star.svg';
+  static const settings = '${_tasks}settings.svg';
+  static const article = '${_tasks}article.svg';
+  static const help = '${_tasks}help.svg';
+  static const terms = '${_tasks}terms.svg';
+  static const question = '${_tasks}question.svg';
+}
+const _images = 'assets/images/images';
+sealed class AppImages{
+  static const rozan = '${_images}rozan.png';
+
 }
--- lib/features/tasks/presentation/tasks_screen.dart
+++ lib/features/tasks/presentation/tasks_screen.dart
@@ -27,72 +27,297 @@ class _TaskScreenState extends State<TaskScreen>
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          drawer: const Drawer(),
-          appBar: AppBar(
-            automaticallyImplyLeading: false,
-            titleSpacing: 0,
-            toolbarHeight: 116,
-            title: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+        drawer: Drawer(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(16, 20, 16, 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  width: 244,
+                  height: 72,
+                  color: scaffoldBackgroundColor,
+                  child: Column(
                     children: [
-                      Builder(builder: (context) {
-                        return GestureDetector(
-                          onTap: () {
-                            Scaffold.of(context).openDrawer();
-                          },
-                          child: SvgPicture.asset(AppIcons.hamburger),
-                        );
-                      }),
-                      const Spacer(),
-                      SvgPicture.asset(AppIcons.note),
-                      const SizedBox(width: 24),
-                      SvgPicture.asset(AppIcons.notification),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [SvgPicture.asset(AppIcons.sun)],
+                      ),
+                      Row(
+                        children: [
+                          Image.asset("assets/images/rozan.png"),
+                          const SizedBox(
+                            width: 8,
+                          ),
+                          const Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Rozan",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontFamily: "Barlow",
+                                      fontWeight: FontWeight.w700),
+                                ),
+                                SizedBox(
+                                  height: 4,
+                                ),
+                                Text(
+                                  "hozar.rasan.matar115@gmail.com",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontFamily: "Barlow",
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                  softWrap: false,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      )
                     ],
                   ),
-                  const SizedBox(height: 16),
-                  TextField(
-                    cursorColor: cursorColor,
-                    decoration: InputDecoration(
-                      contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 44, vertical: 13.5),
-                      prefixIcon: Container(
-                        padding: const EdgeInsets.all(10),
-                        child: SvgPicture.asset(
-                          AppIcons.search,
+                ),
+                const SizedBox(
+                  height: 36,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          SvgPicture.asset(
+                            AppIcons.star,
+                          ),
+                          const SizedBox(
+                            width: 12,
+                          ),
+                          const Text(
+                            "Premium",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontFamily: "Barlow",
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 22.5,
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: SvgPicture.asset(
+                              AppIcons.settings,
+                              width: 20,
+                              height: 20,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 12,
+                          ),
+                          const Text(
+                            "Settings",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontFamily: "Barlow",
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 30.5,
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(4, 2, 4, 2),
+                            child: SvgPicture.asset(
+                              AppIcons.article,
+                              width: 16,
+                              height: 20,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 12,
+                          ),
+                          const Text(
+                            "Articles",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontFamily: "Barlow",
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 18.5,
+                      ),
+                      const Divider(
+                        height: 0.5,
+                      ),
+                      const SizedBox(
+                        height: 18.5,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 10),
+                        child: Row(
+                          children: [
+                            SvgPicture.asset(
+                              AppIcons.help,
+                              width: 24,
+                              height: 24,
+                            ),
+                            const SizedBox(
+                              width: 12,
+                            ),
+                            const Text(
+                              "Help",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontFamily: "Barlow",
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      suffixIcon: Container(
-                        padding: const EdgeInsets.all(10),
-                        child: SvgPicture.asset(AppIcons.filter),
+                      const SizedBox(
+                        height: 29,
                       ),
-                      filled: true,
-                      fillColor: textFieldBackgroundColor,
-                      hintText: 'Search by events, tasks.. ',
-                    ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: SvgPicture.asset(
+                              AppIcons.terms,
+                              width: 20,
+                              height: 20,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 12,
+                          ),
+                          const Text(
+                            "Terms",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontFamily: "Barlow",
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 29,
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: SvgPicture.asset(
+                              AppIcons.question,
+                              width: 20,
+                              height: 20,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 12,
+                          ),
+                          const Text(
+                            "FAQ",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontFamily: "Barlow",
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            bottom: TabBar(
-              controller: tabController,
-              tabs: const [
-                Tab(child: Text("Upcoming")),
-                Tab(child: Text("All")),
+                )
               ],
             ),
           ),
-          body: TabBarView(
+        ),
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          titleSpacing: 0,
+          toolbarHeight: 116,
+          title: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Builder(builder: (context) {
+                      return GestureDetector(
+                        onTap: () {
+                          Scaffold.of(context).openDrawer();
+                        },
+                        child: SvgPicture.asset(AppIcons.hamburger),
+                      );
+                    }),
+                    const Spacer(),
+                    SvgPicture.asset(AppIcons.note),
+                    const SizedBox(width: 24),
+                    SvgPicture.asset(AppIcons.notification),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                TextField(
+                  cursorColor: cursorColor,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 44, vertical: 13.5),
+                    prefixIcon: Container(
+                      padding: const EdgeInsets.all(10),
+                      child: SvgPicture.asset(
+                        AppIcons.search,
+                      ),
+                    ),
+                    suffixIcon: Container(
+                      padding: const EdgeInsets.all(10),
+                      child: SvgPicture.asset(AppIcons.filter),
+                    ),
+                    filled: true,
+                    fillColor: textFieldBackgroundColor,
+                    hintText: 'Search by events, tasks.. ',
+                  ),
+                ),
+              ],
+            ),
+          ),
+          bottom: TabBar(
             controller: tabController,
-            children: const [
-              UpcomingPage(),
-              AllPage(),
+            tabs: const [
+              Tab(child: Text("Upcoming")),
+              Tab(child: Text("All")),
             ],
-          )),
+          ),
+        ),
+        body: TabBarView(
+          controller: tabController,
+          children: const [
+            UpcomingPage(),
+            AllPage(),
+          ],
+        ),
+      ),
     );
   }
 }
